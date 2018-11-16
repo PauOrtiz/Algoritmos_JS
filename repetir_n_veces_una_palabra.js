5aeb2e5edafda01bf81c0a1cf5c1236ab7129087
Paula Ortiz <sarkofy@hotmail.com>
2018-11-16 16:08:36 -0600

    Explicación del algoritmo

--- repetir_n_veces_una_palabra.js
+++ repetir_n_veces_una_palabra.js
@@ -1,9 +1,9 @@
 function repeatStringNumTimes(str, num) {
-  var almacen=""; //sin espacio enmedio ya que es una cadena
-if(num<0){
+  var almacen=""; //sin espacio enmedio ya que contendrá cadena
+if(num<0){//regresa vacío si es negativo
 return almacen;}
 for(var i=0; i<num;i++){
-     almacen=almacen+str;
+     almacen=almacen+str;//va agregando la cadena a la variable
   }
   return almacen;
 }
