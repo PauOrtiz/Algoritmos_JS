67c480bde6ea896395d8ec661d6a56e74c9b0910
Paula Ortiz <sarkofy@hotmail.com>
2018-11-16 14:43:44 -0600

    Factorial de n js

---
+++ factorial_de_un_número.js
@@ -0,0 +1,9 @@
+function factorialize(num) { 
+    
+      var f=1;
+        for (var i = 1; i <= num; i++) {  
+           f = 1 * 1(i) = 1    // f = 1 * 2 = 2\n    // f = 2 * 3 = 6\n    // f = 6 * 4 = 24\n    // ...\n    
+           f= f * i;  }
+             return f;
+             }
+             factorialize(5);
